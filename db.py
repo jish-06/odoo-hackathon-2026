1160f6fe6eaef9f6e08465c0952bd4abc895b1a8
jish-06 <jiya2019shah@gmail.com>
2026-07-12 09:18:29 +0530

    Added all databases

---
+++ db.py
@@ -0,0 +1,81 @@
+import sqlite3
+DB = "data.db"
+def get_db():
+    conn = sqlite3.connect(DB)
+    conn.row_factory = sqlite3.Row
+    return conn
+def init_db():
+    with get_db() as conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL,
+                role TEXT DEFAULT 'employee',
+                department_id INTEGER,
+                status TEXT DEFAULT 'active'
+            )
+        """)
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                head_id INTEGER,
+                status TEXT DEFAULT 'active'
+            )
+        """)
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS asset_categories (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            )
+        """)
+
+        # assets
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS assets (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                asset_tag TEXT UNIQUE NOT NULL,
+                category_id INTEGER,
+                serial_number TEXT,
+                condition TEXT DEFAULT 'Good',
+                location TEXT,
+                status TEXT DEFAULT 'Available',
+                is_bookable INTEGER DEFAULT 0
+            )
+        """)
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS allocations (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                asset_id INTEGER NOT NULL,
+                user_id INTEGER NOT NULL,
+                allocated_date TEXT NOT NULL,
+                expected_return_date TEXT,
+                actual_return_date TEXT,
+                status TEXT DEFAULT 'Active',
+                notes TEXT
+            )
+        """)
+
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS maintenance_requests (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                asset_id INTEGER NOT NULL,
+                user_id INTEGER NOT NULL,
+                description TEXT NOT NULL,
+                priority TEXT DEFAULT 'Medium',
+                status TEXT DEFAULT 'Pending'
+            )
+        """)
+
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS notifications (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                message TEXT NOT NULL,
+                is_read INTEGER DEFAULT 0,
+                created_at TEXT
+            )
+        """)
